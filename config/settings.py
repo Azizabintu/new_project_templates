fd628041c10addf441a533797ce249740eca493c
Azizabintu <aziza.babashova@mail.ru>
2023-04-05 01:48:05 +0500

    static files added and worked state

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('/static'))]
+STATICFILES_DIRS = [BASE_DIR/"static"]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 #STATICFILES_FINDERS = [
 #   'django.contrib.staticfiles.finders.FileSystemFinder',
